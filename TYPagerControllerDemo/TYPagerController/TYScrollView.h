259d0d548fd526dcfc68cb1ab826660fb1b1bfcc
徐家沛 <36961856@qq.com>
2019-03-14 17:05:47 +0800

    加入右滑返回

---
+++ TYPagerControllerDemo/TYPagerController/TYScrollView.h
@@ -0,0 +1,17 @@
+//
+//  TYScrollView.h
+//  TYPagerControllerDemo
+//
+//  Created by 徐家沛 on 2019/3/14.
+//  Copyright © 2019年 tany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TYScrollView : UIScrollView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TYPagerControllerDemo/TYPagerController/TYScrollView.m
@@ -0,0 +1,21 @@
+//
+//  TYScrollView.m
+//  TYPagerControllerDemo
+//
+//  Created by 徐家沛 on 2019/3/14.
+//  Copyright © 2019年 tany. All rights reserved.
+//
+
+#import "TYScrollView.h"
+
+@implementation TYScrollView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end

2b15a2bcb57ef13876c8b3e88ab40efa6140ef37
徐家沛 <36961856@qq.com>
2020-01-09 17:23:35 +0800

    增加两个类

--- TYPagerControllerDemo/TYPagerController/TYScrollView.m
+++ TYPagerControllerDemo/TYPagerController/TYScrollView.m
@@ -10,56 +10,72 @@
 
 @implementation TYScrollView
 
--(BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer{
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer
+{
     CGFloat width = self.contentSize.width - self.frame.size.width;
-    
+
 //    NSLog(@"width = %f, offsetX = %f",width, self.contentOffset.x);
 //    NSLog(@"offsetX = %f gestureRecognizer = %@, otherGestureRecognizer = %@ ", self.contentOffset.x,gestureRecognizer,otherGestureRecognizer);
-    
+
 //    NSLog(@"otherGestureRecognizer.view = %@", NSStringFromClass(otherGestureRecognizer.view.class));
-    
-    if ([NSStringFromClass(otherGestureRecognizer.view.class) isEqualToString:@"UITableView"]) {
+
+    if ([NSStringFromClass(otherGestureRecognizer.view.class) isEqualToString:@"UITableView"])
+    {
         return NO;
     }
-    
-    if ([NSStringFromClass(otherGestureRecognizer.view.class) isEqualToString:@"UICollectionView"]) {
+
+    if ([NSStringFromClass(otherGestureRecognizer.view.class) isEqualToString:@"UICollectionView"])
+    {
         return NO;
     }
-    
-    if ([NSStringFromClass(otherGestureRecognizer.view.class) isEqualToString:@"TYScrollView"]) {
+
+    if ([NSStringFromClass(otherGestureRecognizer.view.class) isEqualToString:@"TYScrollView"])
+    {
         return NO;
     }
-    
-    if ([NSStringFromClass(otherGestureRecognizer.view.class) isEqualToString:@"PXCollectionView"]) {
+
+    if ([NSStringFromClass(otherGestureRecognizer.view.class) isEqualToString:@"PXCollectionView"])
+    {
         return NO;
     }
-    
-    if ([NSStringFromClass(otherGestureRecognizer.view.class) isKindOfClass:UIScrollView.class]) {
+
+    if ([NSStringFromClass(otherGestureRecognizer.view.class) isEqualToString:@"TYCollectionView"])
+    {
         return NO;
     }
-    
-    NSUserDefaults * defaults = [NSUserDefaults standardUserDefaults];
-    
-    NSArray * allLanguages = [defaults objectForKey:@"AppleLanguages"];
-    
-    NSString * preferredLang = [allLanguages objectAtIndex:0];
-    
-    if(preferredLang.length >= 2 && [[preferredLang substringToIndex:2] isEqualToString:@"ar"]){
-        if (self.contentOffset.x >= width) {
+
+    if ([NSStringFromClass(otherGestureRecognizer.view.class) isEqualToString:@"PXTableView"])
+    {
+        return NO;
+    }
+
+    if ([NSStringFromClass(otherGestureRecognizer.view.class) isEqualToString:@"UIScrollView"])
+    {
+        return NO;
+    }
+
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+
+    NSArray *allLanguages = [defaults objectForKey:@"AppleLanguages"];
+
+    NSString *preferredLang = [allLanguages objectAtIndex:0];
+
+    if (preferredLang.length >= 2 && [[preferredLang substringToIndex:2] isEqualToString:@"ar"])
+    {
+        if (self.contentOffset.x >= width)
+        {
             return YES;
         }
-    }else{
-        if (self.contentOffset.x <= 0) {
+    }
+    else
+    {
+        if (self.contentOffset.x <= 0)
+        {
             return YES;
         }
     }
-    
-    
-    
-    
-    
+
     return NO;
-    
 }
 
 @end
